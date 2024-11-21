6a66cbf331f0a2f36c149df6b9838f6990fbc276
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-11-21 11:22:36 -0600

    Display user name and profile image in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { MyButton } from "./components/MyButton";
+const user = {
+  name: "Jonathan",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
 
 function App() {
   return (
     <>
-      <h1>Hola Mundo</h1>
-      <MyButton />
+      <h1>{user.name}</h1>
+      <img
+        className="img-thumbnail"
+        src={user.imageUrl}
+        alt={"Foto de " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
     </>
   );
 }
