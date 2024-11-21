ecba9993cc649426815267777196507210a07541
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2024-11-21 12:12:17 -0600

    Replace student list with a button that triggers an alert on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-const alumnos = [
-  { name: "Jonathan", isAlumn: true, id: 1 },
-  { name: "Ever", isAlumn: false, id: 2 },
-  { name: "Jorge", isAlumn: true, id: 3 },
-];
-
 function App() {
-  const listAlumnos = alumnos.map((alumno) => (
-    <li
-      key={alumno.id}
-      style={{
-        color: alumno.isAlumn ? "darkgreen" : "darkred",
-      }}
-    >
-      {alumno.name}
-    </li>
-  ));
+  function handleClick() {
+    alert("Me hiciste click");
+  }
 
-  return <ul>{listAlumnos}</ul>;
+  return <button onClick={handleClick}>Hazme clic</button>;
 }
 
 export default App;
