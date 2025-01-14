563df76cfdf8618ff83297c3210766823517eec1
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2025-01-14 14:29:25 -0600

    Replace FilterableProductTable with Gallery component in App

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { Counter } from "./components/Counter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <Counter value={0} />
+    <App />
   </StrictMode>
 );
