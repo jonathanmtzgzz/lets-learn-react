3cf85c9878bff392abd2216c4866b14d1ce63c4a
jonathanmtzgzz <jonathanmtzgzz@hotmail.com>
2025-01-14 16:11:33 -0600

    Refactor App component to display a personalized task list and move Profile component to a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
-import Gallery from "./Gallery.jsx";
+const baseUrl = "https://i.imgur.com/";
+const person = {
+  name: "Gregorio Y. Zara",
+  imageId: "7vQD0fP",
+  imageSize: "s",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
 
 export default function App() {
-  return <Gallery />;
+  const name = "Hedy Lamarr";
+  return (
+    <div style={person.theme}>
+      <h1>Tareas pendientes de {person.name}</h1>
+      <img
+        className="avatar"
+        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
+        alt={person.name}
+      />
+      <ul>
+        <li>Mejorar el videoteléfono</li>
+        <li>Preparar clases de aeronáutica</li>
+        <li>Trabajar en el motor de alcohol</li>
+      </ul>
+    </div>
+  );
 }
